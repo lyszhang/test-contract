29e2dbaa02a692204903048f87a7e2f08144b21a
macbook <macbook@macbookdeMacBook-Pro.local>
2025-07-29 16:10:44 +0800

    feat: VulnerableDonation

--- token.sol
+++ token.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract VulnerableDonation {
+contract VulnerableDonation222 {
     mapping(address => uint256) public donations;
 
     // 捐赠函数
